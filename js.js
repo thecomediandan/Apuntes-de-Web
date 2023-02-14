1fe681c6caf00ab0a3bce7b9db9e4b9aa0390c55
thecomediandan <danishpurplesnake@gmail.com>
2023-02-13 23:42:48 -0400

    Se termino la parte uno teorica de Javascript
    :D

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-console.log("Hola esta es mi pagina web")
+let nombre = "Daniel";
+let apellido = "Quispe Kuno";
+let soy = `Yo soy ${nombre} ${apellido}`;
+let id1 = 1;
+
+console.time(`Tiempo ${id1}`);
+for (let index = 0; index < 10; index++) {
+    console.groupCollapsed("Grupo");
+    console.log(nombre);
+    console.log(apellido);
+    console.groupEnd("Grupo");
+    console.timeLog(`Tiempo ${id1}`);
+}
+console.timeEnd(`Tiempo ${id1}`);
