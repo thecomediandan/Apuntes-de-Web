d5783a098689dc295c6508d6d5bdbfcab7497319
thecomediandan <122990427+thecomediandan@users.noreply.github.com>
2023-04-23 00:22:25 -0400

    Avanzando Jaascript Master

--- js/js-mas.js
+++ js/js-mas.js
@@ -1,17 +1,13 @@
 "strict mode";
-
-const validarEdad = (err)=>{
-    let edad;
-    try {
-        if (err) edad = prompt(err);
-        else edad = prompt("introduce tu edad");
-        edad = parseInt(edad);
-        if (isNaN(edad)) throw "introduce un numero para tu edad";
-        if (edad > 18) console.log("sos mayor de edad");
-        else console.log("eres menor de edad");
-    } catch (e) {
-        validarEdad(e);
-    }
-}
-
-validarEdad();
+// Las condicionales pueden tratarse en una linea
+// gastan menos recursos que los if
+// Si hay mas de una linea de operacion van (b1,b2,...,bn)
+let edad = 30;
+(edad > 18) ? 
+// Bloque por verdadero
+(
+    console.log('AFIRMATIVO'),
+    console.log('Es mayor de edad')
+):
+// Bloque por falso
+console.log('NEGATIVO');
