df6b6a258cdbfceef8ac9011f9fca23cfa234241
danyfylth <91761544+danyfylth@users.noreply.github.com>
2023-04-24 00:09:12 -0400

    Agregando informacion al word Javascript-master de Historial

--- js/js-mas.js
+++ js/js-mas.js
@@ -1,13 +1,27 @@
 "strict mode";
-// Las condicionales pueden tratarse en una linea
-// gastan menos recursos que los if
-// Si hay mas de una linea de operacion van (b1,b2,...,bn)
-let edad = 30;
-(edad > 18) ? 
-// Bloque por verdadero
-(
-    console.log('AFIRMATIVO'),
-    console.log('Es mayor de edad')
-):
-// Bloque por falso
-console.log('NEGATIVO');
+// Vas atrás de una página
+history.back();
+// Vas adelante de una página
+history.forward();
+// Tamaño del historial
+history.length
+// Vas a sitios -1=atras, 0 o nada=refresca, 1=adelante
+history.go(-1);
+history.go(0);
+history.go();
+history.go(1);
+// Modifica la URL y conserva la info (state,title,url)
+// guarda entradas en el historial
+history.pushState({nombre: "daniel"}, "", "?modificacion");
+
+// Utilizamos el evento popstate para apenas volvamos a
+// las rutas que modificamos nos retorne el estado de dicha
+// ruta modificada.
+window.addEventListener("popstate", (e)=>{
+    // Mostramos lo que hay en estado
+    console.log(e.state);
+});
+
+// Modifica la URL
+// No guarda entradas en el historial
+history.replaceState({nombre: "daniel"}, "", "?modificacion");
