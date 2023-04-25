c52cd7d5427d634ee3a4ab7636a6be8d41f13afa
danyfylth <91761544+danyfylth@users.noreply.github.com>
2023-04-25 03:44:02 -0400

    Continuando con el llenado del documento de Word de Javascript-master en la parte de IndexDB

--- js/js-mas.js
+++ js/js-mas.js
@@ -1,27 +1,82 @@
 "strict mode";
-// Vas atrás de una página
-history.back();
-// Vas adelante de una página
-history.forward();
-// Tamaño del historial
-history.length
-// Vas a sitios -1=atras, 0 o nada=refresca, 1=adelante
-history.go(-1);
-history.go(0);
-history.go();
-history.go(1);
-// Modifica la URL y conserva la info (state,title,url)
-// guarda entradas en el historial
-history.pushState({nombre: "daniel"}, "", "?modificacion");
-
-// Utilizamos el evento popstate para apenas volvamos a
-// las rutas que modificamos nos retorne el estado de dicha
-// ruta modificada.
-window.addEventListener("popstate", (e)=>{
-    // Mostramos lo que hay en estado
-    console.log(e.state);
+// En esta varible IDBRequest alojamos la solucitud de apertura
+// indexedDB.open("nameDatabase", 1) abrimos la db asignando el nombre y su version
+// En caso de no encontrar la base de datos la creará
+const idbRequest = indexedDB.open("nameDatabase", 1);
+// Esto detecta si no se ha creado la db y necesita crearse de nuevo
+// y lo hace, al hacerlo podemos crear los almacenes de objetos
+idbRequest.addEventListener("upgradeneeded", ()=>{
+    console.log("Se actualizó la base de datos");
+    // Obtenenmos la base de datos
+    const db = idbRequest.result;
+    // Creamos el almacen de objetos para que se vaya llenando
+    // automaticamente con una clave autoincrementable
+    // esta puede ser otra con keypath(investigar)
+    db.createObjectStore("nombres",{
+        autoIncrement: true
+    });
+});
+// verifica si la apertura fue satisfactoria al abrir la base de datos
+idbRequest.addEventListener("success", ()=>{
+    console.log("No hubo problemas al abrir la base de datos");
+});
+// Verfica errores en la apertura
+idbRequest.addEventListener("error", ()=>{
+    console.log("Hubo un error al abrir la base de datos");
 });
 
-// Modifica la URL
-// No guarda entradas en el historial
-history.replaceState({nombre: "daniel"}, "", "?modificacion");
+// Ejemplo de con que haríamos un CRUD
+const agregarObjeto = objeto => {
+    const db = idbRequest.result;
+    // Necesitamos permisos de transaccion en el almacen de objetos
+    // introducimos nombre del almacen y el tipo de transaccion
+    const idbTransaction = db.transaction("nombres", "readwrite");
+    // Identificamos el almacen de objetos con los permisos de transaccion
+    const almacenObjetos = idbTransaction.objectStore("nombres");
+    // Agregamos un objeto
+    almacenObjetos.add(objeto);
+    // Evento para saber si se ha completado la transaccion
+    idbTransaction.addEventListener("complete", ()=>{
+        console.log("Objeto agregado correctamente.");
+    })
+}
+
+const leerObjetos = ()=> {
+    const db = idbRequest.result;
+    const idbTransaction = db.transaction("nombres", "readonly");
+    const almacenObjetos = idbTransaction.objectStore("nombres");
+    // Habilitamos un puntero en el almacen de objetos
+    const puntero = almacenObjetos.openCursor();
+    // Si no hay problemas con el puntero el evento success lo detecta
+    puntero.addEventListener("success", ()=>{
+        // hasta que el puntero sea nulo iterara gracias al metodo continue y
+        // el evento success
+        if (puntero.result) {
+            console.log(puntero.result.value);
+            // El metodo continue permite con la lectura de los
+            // demas elementos moviendo el cursor al siguiente
+            puntero.result.continue();
+        } else console.log("Se terminó de leer los datos.");
+    })
+}
+
+const modificarObjeto = (objeto, clave) => {
+    const db = idbRequest.result;
+    const idbTransaction = db.transaction("nombres", "readwrite");
+    const almacenObjetos = idbTransaction.objectStore("nombres");
+    // En caso que put no encuentre el objeto lo crea
+    almacenObjetos.put(objeto, clave);
+    idbTransaction.addEventListener("complete", ()=>{
+        console.log("Objeto modificado/agregado correctamente.");
+    })
+}
+
+const eliminarObjeto = clave => {
+    const db = idbRequest.result;
+    const idbTransaction = db.transaction("nombres", "readwrite");
+    const almacenObjetos = idbTransaction.objectStore("nombres");
+    almacenObjetos.delete(clave);
+    idbTransaction.addEventListener("complete", ()=>{
+        console.log("Objeto eliminado correctamente.");
+    })
+}
