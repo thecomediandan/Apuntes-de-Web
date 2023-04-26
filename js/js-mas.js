6c22014510c2bffa91ba54c57b3283f910dbd8da
thecomediandan <122990427+thecomediandan@users.noreply.github.com>
2023-04-26 02:41:56 -0400

    Se agrego contenido a javascript-master la parte de notification

--- js/js-mas.js
+++ js/js-mas.js
@@ -1,24 +1,19 @@
 "strict mode";
-// INTERSECTION OBSERVER nos detecta si un elemento es visible
-// o no en el Viewport
-// Seleccionamos diversos elementos para esta prueba
-const elementos = document.querySelectorAll(".classNameElements");
-// Funcion que se llamara a si misma en el observador
-const verificacionVisibilidad = (entradas) => {
-    for (const entrada of entradas) {
-        if (entrada.isIntersecting) {
-            console.log("Se esta viendo el elemento.");
-        }
+// Veficamos si las notificaciones son admitidas por el navegador
+if (!('Notification' in window)) {
+    console.log("Las notificaciones no estan disponiles en tu navegador.");
+}
+// Preguntamos si queremos activar las notificaciones al usuario
+Notification.requestPermission(()=>{
+    if (Notification.permission == "granted") {
+        console.log("Las notificaciones han sido activadas.");
+        let nota = new Notification("Notificandonte un hola :D");
+        console.log(nota);
     }
-}
-// Opciones del observador
-const opciones = {
-    root: "viewport", // Si no se pone nada por defecto es viewport, pero es la referencia que se toma para observar culaquier elemento
-    rootMargin: "30px 30px 30px 30px", // margin del root en este caso viewport, modifica el área de referencia del obnservador
-    treshold: 0.5 // Cuanto debería ocupar el elemento, 0=ni bien toque el borde, 1=hasta que se muestre por conmpleto
-}
-// Observador en este caso esta observando diversos elementos
-const observador = new IntersectionObserver(verificacionVisibilidad, opciones);
-for (const elemento of elementos) {
-   observador.observe(elemento); 
-}
+    if (Notification.permission == "denied") {
+        console.log("Las notificaciones han sido denegadas.");
+    }
+    if (Notification.permission == "default") {
+        console.log("Las notificaciones estan configuradas por defecto.");
+    }
+})
