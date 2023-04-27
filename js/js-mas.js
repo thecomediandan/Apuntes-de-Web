7bed0d44d58fcad75f9990888edfda5ba09bb956
danyfylth <91761544+danyfylth@users.noreply.github.com>
2023-04-26 22:49:32 -0400

    Avanzando con el archivo word Javacript-Master en la parte de Service Worker

--- js/js-mas.js
+++ js/js-mas.js
@@ -1,19 +1,38 @@
 "strict mode";
-// Veficamos si las notificaciones son admitidas por el navegador
-if (!('Notification' in window)) {
-    console.log("Las notificaciones no estan disponiles en tu navegador.");
-}
-// Preguntamos si queremos activar las notificaciones al usuario
-Notification.requestPermission(()=>{
-    if (Notification.permission == "granted") {
-        console.log("Las notificaciones han sido activadas.");
-        let nota = new Notification("Notificandonte un hola :D");
-        console.log(nota);
-    }
-    if (Notification.permission == "denied") {
-        console.log("Las notificaciones han sido denegadas.");
-    }
-    if (Notification.permission == "default") {
-        console.log("Las notificaciones estan configuradas por defecto.");
-    }
-})
+// Script Principal, navegador
+// Verificamos si el navegador admite Service Worker
+if (navigator.serviceWorker) {
+    // Registramos el archivo que nos servirá de Service Worker
+    navigator.serviceWorker.register("nombreServiceWorker.js");
+};
+// Para enviarle un mensaje al Service Worker necesitamos saber si esta listo con el metodo ready el cual devuelve una promesa, el cual tiene una propiedad llamada active que contiene el metodo postMessage.
+navigator.serviceWorker.ready.then(
+    res => res.active.postMessage("Saludando al Service Worker.")
+);
+// Para que el navegador escuche los mensajes del Service Worker
+navigator.serviceWorker.addEventListener("message", e=>{
+    console.log("Mensaje recibido del Service Worker:");
+    console.log(e.data);
+});
+
+////////////////////////////////////////////////////////
+// Service Worker (nombreServiceWorker.js)
+// Para señalar el Service Worker se utiliza self algo como el this
+self.addEventListener("install", e=>{
+    console.log("Service Worker instalado.");
+});
+
+self.addEventListener("activate", ()=>{
+    console.log("Service Worker está activo.");
+});
+
+self.addEventListener("fetch", ()=>{
+    console.log("Service Worker interceptando peticion.");
+});
+
+self.addEventListener("message", e=>{
+    console.log("Mensaje recibido del navegador:");
+    console.log(e.data);
+    // Para enviar un mensaje desde el Service Worker, accedemos al metodo source
+    e.source.postMessage("Saludando al navegador.")
+});
